f10826131b72b3eedb2c2ad4c4d922b7427b7afd
DongrockOh <87560481+DongrockOh@users.noreply.github.com>
2021-09-23 14:36:38 +0900

    #2.2 ~ #2.5 update
    2.2 - Variable
    2.3 const and let
    2.4 Boolean
    2.5 Arrays

---
+++ app.js
@@ -0,0 +1,49 @@
+// ---------- Variables
+
+// const a = 5;
+// const b = 2;
+// let myName = "DR";
+
+// console.log(6 + a);
+// console.log(6 * a);
+// console.log(6 / a);
+// console.log("Hello " + myName);
+
+// myName = "DongRock";
+
+// console.log("your new name is " + myName);
+
+// ---------- Boolean - true or false
+
+// undefined=값이 정의되지 않음
+// null=값이 없음
+// const amIFat = ture; // "" 작성하게되면 String
+// let something; // undefined
+// console.log(amIFat);
+
+// ------------- 2.5 Arrays 
+
+// const mon = "mon";
+// const tue = "tue";
+// const wed = "wed";
+// const thu = "thu";
+// const fri = "fri";
+// const sat = "sat";
+// const sun = "sun";
+
+// 배열을 만들기위해서는 [] 안에 문자열을 입력 해야함. 각 항목간의 , 입력해야함.
+// const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// console.log(daysOfWeek); // 배열항목 전체를 볼 수 있음.
+
+// -- Get Item from Array
+// console.log(daysOfWeek);
+
+// -- Add one more day to the array -
+// daysOfWeek.push("sun"); // 배열 맨뒤에 "sun"을 추가
+// console.log(daysOfWeek); 
+
+// const toBuy = ["potato", "tomato", "pizza"];
+// toBuy.push("kimbab");
+
+// ---------#2.6
