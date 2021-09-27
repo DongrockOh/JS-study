0cedd0aeb5195d4c64deb7600413de10c77c2a22
DongrockOh <87560481+DongrockOh@users.noreply.github.com>
2021-09-27 13:27:00 +0900

    #4.4
    Hello - 문자 // username - 변수명
    
    1 - "Hello " + username
    2 - `Hello ${username}`
    
    1과 2 는 같다.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 //preventDefault - 브라우저의 기본동작을 막아줌.
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  // greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
