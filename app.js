c0e77dc2b20e55655a3fb5093ce8ac3833c660cb
DongrockOh <87560481+DongrockOh@users.noreply.github.com>
2021-09-27 13:53:11 +0900

    #4.5 Saving Username
    localStorage - 우리가 브라우저에서 뭔가를 저장할 수 있게 해줌.
    localStorage ("key", "value")
    .removeItem - 삭제
    .getItem - 불러옴
    .setItem - 저장

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const HIDDEN_CLASSNAME = "hidden";
 
 //preventDefault - 브라우저의 기본동작을 막아줌.
 function onLoginSubmit(event) {
-  event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
+  event.preventDefault(); // 기본동작을 막음.
+  loginForm.classList.add(HIDDEN_CLASSNAME); // form에 hidden class 추가
+  const username = loginInput.value; // input값을 username에 변수에 저장
+  localStorage.setItem("username", username); // setItem(저장)("username"(key),username(변수))
   // greeting.innerText = "Hello " + username;
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`; // greeting변수에 Hello + username 텍스트 삽입.
+  greeting.classList.remove(HIDDEN_CLASSNAME); // h1의 hidden class를 삭제
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
