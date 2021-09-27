2f28d46fa72f32c8c82839402a236aa65ef19183
DongrockOh <87560481+DongrockOh@users.noreply.github.com>
2021-09-27 14:13:53 +0900

    #4.6 Loading Username
    localStorage에 저장한 것을 불러오는 방법.
    paintGreetings  - 중복코드를 묶어줌

--- app.js
+++ app.js
@@ -3,16 +3,30 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
 
 //preventDefault - 브라우저의 기본동작을 막아줌.
 function onLoginSubmit(event) {
   event.preventDefault(); // 기본동작을 막음.
   loginForm.classList.add(HIDDEN_CLASSNAME); // form에 hidden class 추가
   const username = loginInput.value; // input값을 username에 변수에 저장
-  localStorage.setItem("username", username); // setItem(저장)("username"(key),username(변수))
+  localStorage.setItem(USERNAME_KEY, username); // setItem(저장)("username"(key),username(변수))
   // greeting.innerText = "Hello " + username;
-  greeting.innerText = `Hello ${username}`; // greeting변수에 Hello + username 텍스트 삽입.
-  greeting.classList.remove(HIDDEN_CLASSNAME); // h1의 hidden class를 삭제
+  paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // show the greetings
+  paintGreetings(savedUsername);
+}
