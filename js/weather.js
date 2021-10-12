ff6ef76f3337a4a7beb4198a92a457d0f19d7902
DongrockOh <87560481+DongrockOh@users.noreply.github.com>
2021-10-12 15:27:24 +0900

    #8.1 Weather API
    openweathermap.org - site를 통해 api를 전달받아서 위도 경도를 입력하면 지역의 날씨 정보를 얻음.
    promise -지금이 아닌 시간이 조금 지난뒤 발생

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "917ad46fcc88b50c4a674ebcee37c39b";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live in",lat, lng);
+  const lon = position.coords.longitude;
+  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    const weather = document.querySelector("#weather span:first-child");
+    const city = document.querySelector("#weather span:last-child");
+    city.innerText = data.name;
+    weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+  });
+  // url을 fetch하고 그 다음으로 response을 받고 response의 json을 얻음
 }
 function onGeoError() {
   alert("Can't find you. No weather for you.");
